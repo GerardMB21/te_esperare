3b5669644017dcf4fcac3baa9bffd1bc1543f7d1
B_Yoda04 <gerardmatos1990@gmail.com>
2022-11-20 16:15:34 -0500

    add rel=noreferrer in making_of page

--- pages/making_of.tsx
+++ pages/making_of.tsx
@@ -7,7 +7,7 @@ export default function making_of() {
         <Title />
         <div className="content">
           <h1>Making Of</h1>
-          <p>Parte de cada uno de nuestros proyectos ha sido el riguroso proceso de grabación. Es por eso que hemos creado nuestro Tik Tok, <a href="https://www.tiktok.com/@makingof_tesperare" target={"_blank"}>@makingof_tesperare</a>. Aquí, nuestro conductor Edú nos presenta a cada uno de los integrantes del equipo de producción y ellos nos chismean qué opinan sobre el momento.</p>
+          <p>Parte de cada uno de nuestros proyectos ha sido el riguroso proceso de grabación. Es por eso que hemos creado nuestro Tik Tok, <a href="https://www.tiktok.com/@makingof_tesperare" target={"_blank"} rel="noreferrer">@makingof_tesperare</a>. Aquí, nuestro conductor Edú nos presenta a cada uno de los integrantes del equipo de producción y ellos nos chismean qué opinan sobre el momento.</p>
         </div>
       </div>
     </div>
